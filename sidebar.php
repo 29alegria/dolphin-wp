09b5a993216a54f513fc899b89391744c822db07
29alegria <29alegria.leso@gmail.com>
2024-12-14 11:40:26 +0900

    blogページのサイドバーをテンプレートパーツファイル化

---
+++ sidebar.php
@@ -0,0 +1,141 @@
+<aside class="m_sidebar">
+          <div class="m_sidebar-box">
+            <div class="m_sidebar-box_head">
+              <h2 class="m_sidebar-box_head-txt">新着記事</h2>
+            </div>
+            <!-- /.m_sidebar-box_head -->
+            <div class="m_sidebar-box_contents">
+              <article class="m_sidebar-post">
+                <a href="single-blog.html" class="m_sidebar-post_link">
+                  <div class="m_sidebar-post_thumb-wrapper">
+                    <img
+                      width="600"
+                      height="400"
+                      src="<?php echo esc_url(get_template_directory_uri() . "/assets/images/blog-thumbnail.jpg"); ?>"
+                      alt="ブログサムネイル"
+                      class="m_sidebar-post_thumb"
+                    />
+                  </div>
+                  <!-- /.m_post_thumb-wrapper -->
+                  <div class="m_sidebar-post_content">
+                    <div class="m_sidebar-post_meta">
+                      <time class="m_sidebar-post_meta-date">2024.12.10</time>
+                      <span class="m_sidebar-post_meta-cat">カテゴリー</span>
+                    </div>
+                    <!-- ./m_post_meta -->
+                    <h2 class="m_sidebar-post_title">
+                      未経験から今すぐ始められる！交通誘導警備の魅力とは？未経験者でも安心のステップアップ術
+                    </h2>
+                  </div>
+                  <!-- /.m_post_content -->
+                </a>
+              </article>
+              <article class="m_sidebar-post">
+                <a href="single-blog.html" class="m_sidebar-post_link">
+                  <div class="m_sidebar-post_thumb-wrapper">
+                    <img
+                      width="600"
+                      height="400"
+                      src="<?php echo esc_url(get_template_directory_uri() . "/assets/images/blog-thumbnail.jpg"); ?>"
+                      alt="ブログサムネイル"
+                      class="m_sidebar-post_thumb"
+                    />
+                  </div>
+                  <!-- /.m_post_thumb-wrapper -->
+                  <div class="m_sidebar-post_content">
+                    <div class="m_sidebar-post_meta">
+                      <time class="m_sidebar-post_meta-date">2024.12.10</time>
+                      <span class="m_sidebar-post_meta-cat">カテゴリー</span>
+                    </div>
+                    <!-- ./m_post_meta -->
+                    <h2 class="m_sidebar-post_title">
+                      未経験から今すぐ始められる！交通誘導警備の魅力とは？未経験者でも安心のステップアップ術
+                    </h2>
+                  </div>
+                  <!-- /.m_post_content -->
+                </a>
+              </article>
+              <article class="m_sidebar-post">
+                <a href="single-blog.html" class="m_sidebar-post_link">
+                  <div class="m_sidebar-post_thumb-wrapper">
+                    <img
+                      width="600"
+                      height="400"
+                      src="<?php echo esc_url(get_template_directory_uri() . "/assets/images/blog-thumbnail.jpg"); ?>"
+                      alt="ブログサムネイル"
+                      class="m_sidebar-post_thumb"
+                    />
+                  </div>
+                  <!-- /.m_post_thumb-wrapper -->
+                  <div class="m_sidebar-post_content">
+                    <div class="m_sidebar-post_meta">
+                      <time class="m_sidebar-post_meta-date">2024.12.10</time>
+                      <span class="m_sidebar-post_meta-cat">カテゴリー</span>
+                    </div>
+                    <!-- ./m_post_meta -->
+                    <h2 class="m_sidebar-post_title">
+                      未経験から今すぐ始められる！交通誘導警備の魅力とは？未経験者でも安心のステップアップ術
+                    </h2>
+                  </div>
+                  <!-- /.m_post_content -->
+                </a>
+              </article>
+              <article class="m_sidebar-post">
+                <a href="single-blog.html" class="m_sidebar-post_link">
+                  <div class="m_sidebar-post_thumb-wrapper">
+                    <img
+                      width="600"
+                      height="400"
+                      src="<?php echo esc_url(get_template_directory_uri() . "/assets/images/blog-thumbnail.jpg"); ?>"
+                      alt="ブログサムネイル"
+                      class="m_sidebar-post_thumb"
+                    />
+                  </div>
+                  <!-- /.m_post_thumb-wrapper -->
+                  <div class="m_sidebar-post_content">
+                    <div class="m_sidebar-post_meta">
+                      <time class="m_sidebar-post_meta-date">2024.12.10</time>
+                      <span class="m_sidebar-post_meta-cat">カテゴリー</span>
+                    </div>
+                    <!-- ./m_post_meta -->
+                    <h2 class="m_sidebar-post_title">
+                      未経験から今すぐ始められる！交通誘導警備の魅力とは？未経験者でも安心のステップアップ術
+                    </h2>
+                  </div>
+                  <!-- /.m_post_content -->
+                </a>
+              </article>
+
+            </div>
+            <!-- /.m_sidebar-box_head -->
+          </div>
+          <!-- /.m_sidebar-box -->
+          <div class="m_cta__recruit m_cta__recruit__sidebar">
+            <div class="l_container-lg">
+              <div class="top_section_title">
+                <p class="top_section_title_sub">RECRUIT</p>
+                <h2 class="top_section_title_main">採用情報</h2>
+              </div>
+              <!-- /.top_section_title --> 
+              <p class="m_cta__recruit-txt">
+                業務拡大に伴い、新たに警備スタッフを募集いたします！未経験の方も安心して始められる環境を整えています。<br 
+                />警備の仕事にご興味のある方は、ぜひお気軽にご応募ください。
+              </p>
+              <div class="m_recruit_btn-wrapper">
+                <a href="<?php echo esc_url(home_url("/recruit")); ?>" class="m_btn m_recruit_btn m_btn__right-arrow">
+                  <span class="btn_head">採用情報を見る</span>
+                  <div class="m_arrow-wrapper">
+                    <div class="m_arrow__round">
+                      <div class="m_arrow m_arrow__right"></div>
+                    </div>            
+                    <!-- /.m_arrow__round -->
+                  </div>
+                  <!-- /.m_arrow-wrapper -->
+                </a>
+              </div>
+              <!-- /.m_recruit_btn-wrapper -->
+            </div>
+            <!-- /.l_container-lg -->
+          </div>
+          <!-- /.m_cta__recruit -->
+        </aside>
