963145b732a4881d66ae44f502fb39568a80b7f7
29alegria <29alegria.leso@gmail.com>
2024-12-16 08:48:23 +0900

    オープニングアニメーション変更

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,6 +14,13 @@ function openingAnime() {
       scale: 0.8,
       y: 40,
     })
+    .to(".js_opening-logo", {
+      duration: 0.6,
+      y: -8, // 上に移動
+      yoyo: true, // 移動を逆方向に繰り返す
+      repeat: -1, // 無限ループ
+      ease: "power1.inOut", // スムーズなバウンド効果
+    } ,"<")
     .from(".js_opening-txt", {
       duration: 2,
       autoAlpha: 0,
@@ -21,12 +28,14 @@ function openingAnime() {
     })
     .to(".js_opening", {
       autoAlpha: 0,
-      duration: 1.5,
+      duration: 3,
       onComplete: function () {
         body.classList.toggle("is-active");
         underlineAnimation(); // 初回アニメーションの最後に下線アニメーションを実行
+        gsap.killTweensOf(".js_opening-logo"); // バウンドアニメーションを停止
       },
     });
+
 }
 
 // 常時実行する下線のアニメーション
@@ -43,6 +52,8 @@ function underlineAnimation() {
       ease: "power1.out",
     }, "+=0.2"); // 0.2秒遅延
 }
+
+// openingAnime();
 
 // 初回アクセス時の処理
 const opening = document.querySelector(".js_opening");
