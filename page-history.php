890d39661f26d56d73acb9c15034829da148c88d
29alegria <29alegria.leso@gmail.com>
2025-01-30 17:43:55 +0900

    history追加

--- page-history.php
+++ page-history.php
@@ -16,6 +16,8 @@ if (have_posts()) :
         </div>
         <!-- /.m_section-title -->
         <div class="history_info-boxes">
+
+          <!-- １４ -->
           <div class="history_info-box">
             <h3 class="history_title">平成14年</h3>
             <table class="history-info">
@@ -38,24 +40,358 @@ if (have_posts()) :
             </table>
           </div>
           <!-- /.history_info-box -->
+          
+          <!-- １５ -->
           <div class="history_info-box">
-            <h3 class="history_title">平成14年</h3>
+            <h3 class="history_title">平成15年</h3>
             <table class="history-info">
               <tr>
-                <th>4月</th>
-                <td>千葉県市川市に於いて交通誘導警備業務開始。</td>
+                <th>3月</th>
+                <td>安藤建設株式会社に於いて松原団地新築工事他。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->       
+
+          <!-- １６ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成16年</h3>
+            <table class="history-info">
+              <tr>
+                <th>1月</th>
+                <td>株式会社シミズ・ビルライフケアに於いて日銀本店前菊池ビル改修工事。</td>
               </tr>
               <tr>
                 <th>5月</th>
-                <td>鎌ヶ谷市、柏市、白井市内のパチンコ店他の駐車場警備。</td>
+                <td>株式会社大城組に於いてサニーコート市川駅前新築工事他。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+          
+          <!-- １７ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成17年</h3>
+            <table class="history-info">
+              <tr>
+                <th>7月</th>
+                <td>新日本建設株式会社に於いてクレアトール西日暮里新築工事。</td>
               </tr>
               <tr>
-                <th>8月</th>
-                <td>曹洞宗宝成寺法要。</td>
+                <th>12月</th>
+                <td>大成ユーレック株式会社に於いてハイマート幸大規模修繕工事。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+          
+          <!-- １８ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成18年</h3>
+            <table class="history-info">
+              <tr>
+                <th>6月</th>
+                <td>有志警備会社連合会のNET会参加。</td>
+              </tr>
+              <tr>
+                <th>同月</th>
+                <td>市川市内、鎌ヶ谷市内のスーパーマルエツ駐車場警備。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->   
+
+          <!-- １９ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成19年</h3>
+            <table class="history-info">
+              <tr>
+                <th>3月</th>
+                <td>株式会社大林組に於いて市川市外観道路工事。</td>
+              </tr>
+              <tr>
+                <th>12月</th>
+                <td>安藤建設株式会社に於いてグランドステージ住吉立替事業。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+          
+          <!-- ２０ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成20年</h3>
+            <table class="history-info">
+              <tr>
+                <th>4月</th>
+                <td>株式会社大城組に於いて市川税務署増築工事。</td>
+              </tr>
+              <tr>
+                <th>5月</th>
+                <td>市川市都市計画道路課に於いて八方橋周辺安全管理業務。</td>
+              </tr>
+              <tr>
+                <th>9月</th>
+                <td>株式会社東映テレビ・プロダクションに於いてドラマ『相棒』撮影現場警備。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->    
+                
+          <!-- ２１ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成21年</h3>
+            <table class="history-info">
+              <tr>
+                <th>1月</th>
+                <td>株式会社大城組に於いて県立行徳高校普通教室棟大規模改善建築工事。</td>
+              </tr>
+              <tr>
+                <th>10月</th>
+                <td>清水建設株式会社に於いて三菱ふそうバス・トラック株式会社EOL新築工事。</td>
               </tr>
               <tr>
                 <th>11月</th>
-                <td>私立聖マリア幼稚園バザーに伴う駐車場警備。</td>
+                <td>窯焼きパン工房ル・マタン駐車場警備。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+          
+          <!-- ２２ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成22年</h3>
+            <table class="history-info">
+              <tr>
+                <th>1月</th>
+                <td>株式会社大城組に於いて税務大学校東京研修所庁舎棟建築改修その他工事。</td>
+              </tr>
+              <tr>
+                <th>10月</th>
+                <td>フジミビルサービス株式会社に於いて本八幡A地区第一種市街地再開発事業。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->  
+                  
+          <!-- ２３ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成23年</h3>
+            <table class="history-info">
+              <tr>
+                <th>2月</th>
+                <td>新日本建設株式会社に於いてCONOE一番町新築工事。</td>
+              </tr>
+              <tr>
+                <th>7月</th>
+                <td>市川建設株式会社に於いて市川市立第六中学校耐震補強工事。</td>
+              </tr>
+              <tr>
+                <th>12月</th>
+                <td>前田建設工業株式会社に於いて本八幡A地区第一種市街地再開発工事。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+          
+          <!-- ２４ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成24年</h3>
+            <table class="history-info">
+              <tr>
+                <th>10月</th>
+                <td>学校法人武田学園晴山幼稚園に於いて運動会に伴う駐車場警備。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box --> 
+                
+          <!-- ２５ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成25年</h3>
+            <table class="history-info">
+              <tr>
+                <th>4月</th>
+                <td>積水ハウス株式会社に於いてグランドターミナルタワー本八幡内覧会警備。</td>
+              </tr>
+              <tr>
+                <th>12月</th>
+                <td>イオンモール幕張新都心店に於ける駐車場警備。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+          
+          <!-- ２６ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成26年</h3>
+            <table class="history-info">
+              <tr>
+                <th>1月</th>
+                <td>フジミビルサービス株式会社に於いて流山東葛病院新築工事。</td>
+              </tr>
+              <tr>
+                <th>3月</th>
+                <td>前田建設工業株式会社に於いて東京外環自動車道路市川市田尻地区函渠その２工事。</td>
+              </tr>
+              <tr>
+                <th>9月</th>
+                <td>ふなばしハワイアンフェスティバル実行委員会に於いて同イベント駐車場警備。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+          
+          <!-- ２７ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成27年</h3>
+            <table class="history-info">
+              <tr>
+                <th>7月</th>
+                <td>株式会社KEIYOリニューアルに於いてパークフィールドみさとテラ・ウエスト2街区2号館大改修工事。</td>
+              </tr>
+              <tr>
+                <th>11月</th>
+                <td>菓子工房アントレに於いて本店駐車場警備。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+          
+          <!-- ２８ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成28年</h3>
+            <table class="history-info">
+              <tr>
+                <th>3月</th>
+                <td>株式会社ラクシーに於いて稲毛海岸3丁目団地第3回大規模修繕工事。</td>
+              </tr>
+              <tr>
+                <th>11月</th>
+                <td>株式会社クニハルに於いて浦安・鎌ヶ谷線新設道路開通式に伴う警備。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+            
+          <!-- ２９　-->
+          <div class="history_info-box">
+            <h3 class="history_title">平成29年</h3>
+            <table class="history-info">
+              <tr>
+                <th>1月</th>
+                <td>山英建設株式会社に於いてマミーマート船橋夏見台店新築工事。</td>
+              </tr>
+              <tr>
+                <th>11月</th>
+                <td>株式会社エフビーエス・ミヤマに於いて（仮称）中野区本町4丁目共同住宅新築工事。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->
+            
+          <!-- ３０ -->
+          <div class="history_info-box">
+            <h3 class="history_title">平成17年</h3>
+            <table class="history-info">
+              <tr>
+                <th>5月</th>
+                <td>株式会社大城組に於いて市川市国府台県営住宅A地区新築工事。</td>
+              </tr>
+              <tr>
+                <th>9月</th>
+                <td>株式会社ミライトに於いて八千代市内のケーブルテレビ専用ケーブル交換工事。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->  
+                  
+          <!-- １ -->
+          <div class="history_info-box">
+            <h3 class="history_title">令和元年</h3>
+            <table class="history-info">
+              <tr>
+                <th>3月</th>
+                <td>工営建設株式会社に於いて市川市西浦処理分区管渠布設工事。</td>
+              </tr>
+              <tr>
+                <th>6月</th>
+                <td>株式会社大城組に於いてマルエツ目黒店建替え工事。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->     
+
+          <!-- ２ -->
+          <div class="history_info-box">
+            <h3 class="history_title">令和2年</h3>
+            <table class="history-info">
+              <tr>
+                <th>3月</th>
+                <td>新日本建設株式会社に於いてEXC北小金計画新築工事。</td>
+              </tr>
+              <tr>
+                <th>11月</th>
+                <td>前田建設工業株式会社に於いて市川市文化会館リニューアル工事。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->   
+
+          <!-- ３ -->
+          <div class="history_info-box">
+            <h3 class="history_title">令和3年</h3>
+            <table class="history-info">
+              <tr>
+                <th>1月</th>
+                <td>株式会社大城組に於いて特別養護老人ホーム華つばき新築工事。</td>
+              </tr>
+              <tr>
+                <th>9月</th>
+                <td>株式会社エフビーエスに於いて三笠製薬株式会社本社社屋耐震補強工事。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->   
+
+          <!-- ４ -->
+          <div class="history_info-box">
+            <h3 class="history_title">令和4年</h3>
+            <table class="history-info">
+              <tr>
+                <th>4月</th>
+                <td>前田道路株式会社に於いて電線共同溝設置に伴う道路復旧工事（3六一環3言問）。</td>
+              </tr>
+              <tr>
+                <th>6月</th>
+                <td>株式会社鍜治田工務店に於いて市川市行徳駅前2丁目計画新築工事。水道本管布設替工事。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->          
+          
+          <!-- ５ -->
+          <div class="history_info-box">
+            <h3 class="history_title">令和5年</h3>
+            <table class="history-info">
+              <tr>
+                <th>4月</th>
+                <td>小倉建設株式会社に於いてレフェルム四街道新築工事。</td>
+              </tr>
+              <tr>
+                <th>7月</th>
+                <td>株式会社大城組において（仮称）市川市八幡市民複合施設新築工事。</td>
+              </tr>
+            </table>
+          </div>
+          <!-- /.history_info-box -->    
+                
+          <!-- ６ -->
+          <div class="history_info-box">
+            <h3 class="history_title">令和6年</h3>
+            <table class="history-info">
+              <tr>
+                <th>10月</th>
+                <td>久保田建設株式会社・株式会社中川組JVに於いて市川市北方地区公共下水道整備事業建設工事。</td>
               </tr>
             </table>
           </div>
